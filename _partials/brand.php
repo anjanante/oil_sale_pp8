d9c8aed303da434a387d95a591c453482c049277
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-07-16 16:36:27 +0300

    Initial data from oldskool-html-bootstrap template

---
+++ _partials/brand.php
@@ -0,0 +1,140 @@
+<!-- Featured Brands-->
+<div class="brand-section container-fluid" data-aos="fade-in">
+    <div class="bg-overlay-sides-white-to-transparent bg-white py-5 py-md-7">
+        <section class="marquee marquee-hover-pause">
+            <div class="marquee-body">
+                <div class="marquee-section animation-marquee-50">
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-1.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-2.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-3.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-4.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-5.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-6.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-7.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-8.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-3 mx-lg-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-9.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="marquee-section animation-marquee-50">
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-1.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-2.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-3.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-4.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-5.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-6.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-7.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-8.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="mx-5 f-w-24">
+                        <a class="d-block" href="./category.html">
+                            <picture>
+                                <img class="img-fluid d-table mx-auto" src="./assets/images/logos/logo-9.svg" alt="">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+<!--/ Featured Brands-->
---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+require("_partials/header.php");
+?>
+<!-- / Top banner -->
+<section class="vh-75 vh-lg-60 container-fluid rounded overflow-hidden" data-aos="fade-in">
+    <!-- Swiper Info -->
+    <div class="swiper-container overflow-hidden rounded h-100 bg-light" data-swiper data-options='{
+              "spaceBetween": 0,
+              "slidesPerView": 1,
+              "effect": "fade",
+              "speed": 1000,
+              "loop": true,
+              "parallax": true,
+              "observer": true,
+              "observeParents": true,
+              "lazy": {
+                "loadPrevNext": true
+                },
+                "autoplay": {
+                  "delay": 5000,
+                  "disableOnInteraction": false
+              },
+              "pagination": {
+                "el": ".swiper-pagination",
+                "clickable": true
+                }
+              }'>
+        <div class="swiper-wrapper">
+
+            <!-- Slide-->
+            <div class="swiper-slide position-relative h-100 w-100">
+                <div class="w-100 h-100 overflow-hidden position-absolute z-index-1 top-0 start-0 end-0 bottom-0">
+                    <div class="w-100 h-100 bg-img-cover bg-pos-center-center overflow-hidden" data-swiper-parallax="-100" style=" will-change: transform; background-image: url(./assets/images/banners/banner-1.jpg)">
+                    </div>
+                </div>
+                <div class="container position-relative z-index-10 d-flex h-100 align-items-start flex-column justify-content-center">
+                    <p class="title-small text-white opacity-75 mb-0" data-swiper-parallax="-100">Everything You Need</p>
+                    <h2 class="display-3 tracking-wide fw-bold text-uppercase tracking-wide text-white" data-swiper-parallax="100">
+                        <span class="text-outline-light">Summer</span> Essentials
+                    </h2>
+                    <div data-swiper-parallax-y="-25">
+                        <a href="./category.html" class="btn btn-psuedo text-white" role="button">Shop New Arrivals</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /Slide-->
+
+            <!-- Slide-->
+            <div class="swiper-slide position-relative h-100 w-100">
+                <div class="w-100 h-100 overflow-hidden position-absolute z-index-1 top-0 start-0 end-0 bottom-0">
+                    <div class="w-100 h-100 bg-img-cover bg-pos-center-center overflow-hidden" data-swiper-parallax="-100" style=" will-change: transform; background-image: url(./assets/images/banners/banner-2.jpg)">
+                    </div>
+                </div>
+                <div class="container position-relative z-index-10 d-flex h-100 align-items-start flex-column justify-content-center">
+                    <p class="title-small text-white opacity-75 mb-0" data-swiper-parallax="-100">Oil Collection</p>
+                    <h2 class="display-3 tracking-wide fw-bold text-uppercase tracking-wide text-white" data-swiper-parallax="100">
+                        Bottle <span class="text-outline-light">SS21</span></h2>
+                    <div data-swiper-parallax-y="-25">
+                        <a href="./category.html" class="btn btn-psuedo text-white" role="button">Shop Latest Oil</a>
+                    </div>
+                </div>
+            </div>
+            <!--/Slide-->
+
+            <!-- Slide-->
+            <div class="swiper-slide position-relative h-100 w-100">
+                <div class="w-100 h-100 overflow-hidden position-absolute z-index-1 top-0 start-0 end-0 bottom-0">
+                    <div class="w-100 h-100 bg-img-cover bg-pos-center-center overflow-hidden" data-swiper-parallax="-100" style=" will-change: transform; background-image: url(./assets/images/banners/banner-4.png)">
+                    </div>
+                </div>
+                <div class="container position-relative z-index-10 d-flex h-100 align-items-start flex-column justify-content-center">
+                    <p class="title-small text-white opacity-75 mb-0" data-swiper-parallax="-100">Just Do it</p>
+                    <h2 class="display-3 tracking-wide fw-bold text-uppercase tracking-wide text-white" data-swiper-parallax="100">
+                        Clonane <span class="text-outline-light">SS21</span></h2>
+                    <div data-swiper-parallax-y="-25">
+                        <a href="./category.html" class="btn btn-psuedo text-white" role="button">Shop Latest Oil</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /Slide-->
+
+            <!--Slide-->
+            <div class="swiper-slide position-relative h-100 w-100">
+                <div class="w-100 h-100 overflow-hidden position-absolute z-index-1 top-0 start-0 end-0 bottom-0">
+                    <div class="w-100 h-100 bg-img-cover bg-pos-center-center overflow-hidden" data-swiper-parallax="-100" style=" will-change: transform; background-image: url(./assets/images/banners/banner-3.jpg)">
+                    </div>
+                </div>
+                <div class="container position-relative z-index-10 d-flex h-100 align-items-start flex-column justify-content-center">
+                    <p class="title-small text-white opacity-75 mb-0" data-swiper-parallax="-100">Look Good Feel Good</p>
+                    <h2 class="display-3 tracking-wide fw-bold text-uppercase tracking-wide text-white" data-swiper-parallax="100">
+                        <span class="text-outline-light">Sustainable</span> Oil
+                    </h2>
+                    <div data-swiper-parallax-y="-25">
+                        <a href="./category.html" class="btn btn-psuedo text-white" role="button">Why We Are Different</a>
+                    </div>
+                </div>
+            </div>
+            <!--/Slide-->
+
+        </div>
+
+        <div class="swiper-pagination swiper-pagination-bullet-light"></div>
+
+    </div>
+    <!-- / Swiper Info-->
+</section>
+
+<?php include("_partials/brand.php"); ?>
+
+<div class="container-fluid">
+    <!-- Homepage Banners-->
+    <div class="pt-7 mb-5 mb-lg-10">
+        <div class="row g-4">
+            <div class="col-12 col-xl-6 position-relative" data-aos="fade-right">
+                <picture class="position-relative z-index-10">
+                    <img class="w-100 rounded" src="./assets/images/banners/banner-sale.jpg" alt="HTML Bootstrap Template by Pixel Rocket">
+                </picture>
+                <div class="position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center z-index-20">
+                    <div class="py-6 px-5 px-lg-10 text-center w-100">
+                        <h2 class="display-1 mb-3 fw-bold text-white"><span class="text-outline-light">Flash</span> Sale</h2>
+                        <p class="fs-5 fw-light text-white d-none d-md-block">Our yearly flash sale is now on! Grab yourself a bargain from the world's leading streetwear brands.</p>
+                        <a href="./category.html" class="btn btn-psuedo text-white" role="button">Shop All Sale Items</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-xl-6" data-aos="fade-left">
+                <div class="row g-4 justify-content-end">
+                    <div class="col-12 col-md-6 d-flex">
+                        <div class="card position-relative overflow-hidden">
+                            <picture class="position-relative z-index-10 d-block bg-light">
+                                <img class="w-100 rounded" src="./assets/images/banners/banner-8.jpg" alt="HTML Bootstrap Template by Pixel Rocket">
+                            </picture>
+                            <div class="card-overlay">
+                                <p class="lead fw-bolder mb-2">The Jordan Delta 2</p>
+                                <a href="./category.html" class="btn btn-psuedo text-white py-2" role="button">Shop Kicks</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 d-flex">
+                        <div class="card position-relative overflow-hidden">
+                            <picture class="position-relative z-index-10 d-block bg-light">
+                                <img class="w-100 rounded" src="./assets/images/banners/banner-9.jpg" alt="HTML Bootstrap Template by Pixel Rocket">
+                            </picture>
+                            <div class="card-overlay">
+                                <p class="lead fw-bolder mb-2">Latest Mens Shirts</p>
+                                <a href="./category.html" class="btn btn-psuedo text-white py-2" role="button">Shop New</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 d-flex">
+                        <div class="card position-relative overflow-hidden">
+                            <picture class="position-relative z-index-10 d-block bg-light">
+                                <img class="w-100 rounded" src="./assets/images/banners/banner-10.jpg" alt="HTML Bootstrap Template by Pixel Rocket">
+                            </picture>
+                            <div class="card-overlay">
+                                <p class="lead fw-bolder mb-2">KiiKii Osake Tees</p>
+                                <a href="./category.html" class="btn btn-psuedo text-white py-2" role="button">Shop T-Shirts</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 d-flex">
+                        <div class="card position-relative overflow-hidden">
+                            <picture class="position-relative z-index-10 d-block bg-light">
+                                <img class="w-100 rounded" src="./assets/images/banners/banner-11.jpg" alt="HTML Bootstrap Template by Pixel Rocket">
+                            </picture>
+                            <div class="card-overlay">
+                                <p class="lead fw-bolder mb-2">Multibuy Womens Shirts</p>
+                                <a href="./category.html" class="btn btn-psuedo text-white py-2" role="button">Shop Sale Items</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Homepage Banners-->
+</div>
+<?php
+require("_partials/footer.php");
+?>
